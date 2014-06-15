7388cf5f531353e6db54f11b3f1a74a9fa237dbc
grafinche <grafinche@gmail.com>
2014-06-15 13:32:37 +0300

    fix merging issues.Added function for 'opening' & 'closing' tiles

--- MemoryGameSVG/MemoryGameSVG/memory-game.js
+++ MemoryGameSVG/MemoryGameSVG/memory-game.js
@@ -1,21 +1,15 @@
-var memoryArray = [
+﻿var memoryArray = [
    'red', 'red', 'blue', 'blue', 'gray', 'gray', 'green', 'green', 'yellow', 'yellow', 'purple', 'purple', 'orange', 'orange',
    'pink', 'pink', 'greenyellow', 'greenyellow', 'darkblue', 'darkblue', 'brown', 'brown', 'cyan', 'cyan'];
 
 var redrawn = [];
 var arr = [];
-<<<<<<< HEAD
 var posArr = [];
 
 var logo = new Image();
 logo.src = "images/teleriklogo1.png";
 var xOffset = 0,
      xIncr = 0;
-
-=======
-var logo = new Image();
-logo.src = "images/teleriklogo.png";
->>>>>>> 5855bd9f7b547df5b3d796c9c388469dc72f5c68
 
 Array.prototype.memoryTileShuffle = function () {
     var i = this.length, j, temp;
@@ -74,7 +68,11 @@ function backToOriginal(id) {
 }
 
 function initialize() {
-<<<<<<< HEAD
+    var counterX,
+       rows = 4,
+       cols = 6,
+       i,
+       j;
     for (var i = 0; i < 4; i++) {
         var counterX = 120;
         for (var j = 0; j < 6; j++) {
@@ -84,38 +82,18 @@ function initialize() {
                      //stroke:"yellow",
                      //"stroke-width":4,
                      //"stroke-linejoin": 'round',
-=======
-    var counterX,
-        rows = 4,
-        cols = 6,
-        i,
-        j;
-    for (i = 0; i < rows; i++) {
-        counterX = 110;
-        for (j = 0; j < cols; j++) {
-            arr.push(paper.rect(-90 + counterX, -90 + conterY, 110, 110)
-                 .attr({
-                     fill: "url('/images/teleriklogo.png')",
-					 stroke:"yellow",
-					 "stroke-width":4
->>>>>>> 5855bd9f7b547df5b3d796c9c388469dc72f5c68
+
                  })
                  .click(function () {
                      reDraw(this.id);
                  })
-<<<<<<< HEAD
             );
             context.drawImage(logo, -90 + 142 + counterX, -90 + 30 + conterY);
-=======
-                 );
-            context.drawImage(logo,-90 +142+ counterX, -90 +30+ conterY);
->>>>>>> 5855bd9f7b547df5b3d796c9c388469dc72f5c68
             counterX += 120;
         }
         conterY += 120;
     }
 }
-<<<<<<< HEAD
 
 function scaleDecr() {
     context.clearRect(0, 0, logo.width, logo.height);
@@ -141,5 +119,4 @@ function doTimer(funct) {
 function stopTimer() {
     clearInterval(timerID);
 }
-=======
->>>>>>> 5855bd9f7b547df5b3d796c9c388469dc72f5c68
+
